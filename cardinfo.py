16b41eb1761d5d40e396e16fde8c0ad3d4474c06
Maxim Logunov <spitty.pnz@gmail.com>
2016-01-24 22:26:31 +0300

    Add threshold to CardInfo

--- cardinfo.py
+++ cardinfo.py
@@ -8,8 +8,12 @@ logger = logging.getLogger(__name__)
 class CardInfo:
     def __init__(self, card_number):
         self.card_number = card_number
+        self.threshold = None
         self.json = checker.get_status(self.card_number)
         self._int_update_by_json(self.json)
+
+    def __str__(self):
+        return "Card(%s, balance: %s, threshold: %s" % (self.card_number, self.balance, self.threshold)
 
     def _int_update_by_json(self, json):
         self.last_updated = time()
@@ -23,3 +27,9 @@ class CardInfo:
             return False
         self.json = checker.get_status(self.card_number)
         self._int_update_by_json(self.json)
+
+    def check_threshold_valid(self):
+        if not self.threshold:
+            logger.warn("Threshold is not specified for card %s" % self)
+            return True
+        return self.balance > self.threshold
